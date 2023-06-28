2052cdbfaf47c9c0d9b159b66fae7ae7eb91e37e
akaprasanga <071bct525prasanga@pcampus.edu.np>
2023-06-28 16:18:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,24 +18,3 @@ if file_obj['ResponseMetadata']['HTTPStatusCode'] == 200:
     st.write(df.head())
 else:
     st.write('Error loading data from S3')
-
-st.stop()
-st.write(initial_df)
-# Create connection object.
-# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
-fs = s3fs.S3FileSystem(anon=False)
-
-# Retrieve file contents.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def read_file(filename):
-    with fs.open(filename) as f:
-        return f.read().decode("utf-8")
-
-content = read_file("streamlit-maket-data/test-data/dummy_data.csv")
-
-st.write(content)
-
-# Print results.
-# for row in df.itertuples():
-#     st.write(f"{row.Owner} has a :{row.Pet}:")

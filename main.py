02b5e217d8b61a3b8b5711c3a3486854fe5c5ecc
akaprasanga <071bct525prasanga@pcampus.edu.np>
2023-06-28 13:57:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 from st_files_connection import FilesConnection
-
+st.header("Welcome to the dashboard!")
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.experimental_connection('s3', type=FilesConnection)
